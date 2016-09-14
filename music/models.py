ef1fdb7601959c397c1f26a0f24fdaaaab83717a
Bastien VINH <bus.bvinh@gmail.com>
2016-09-14 22:18:47 +0100

    Add new field

--- music/models.py
+++ music/models.py
@@ -5,6 +5,7 @@ class Album(models.Model):
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
+    is_favourite = models.BooleanField(default=False)
 
     def __str__(self):
         return "name: {0} , album_title: {1}".format(self.artist, self.album_title)
--- music/views.py
+++ music/views.py
@@ -4,13 +4,12 @@ from .models import Album
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
-
+    context_object_name = 'all_albums'
 
     def get_queryset(self):
         return Album.objects.all()
 
 
-class DetailView(generic.ListView):
+class DetailView(generic.DetailView):
     model = Album
     template_name = 'music/detail.html'
-    
