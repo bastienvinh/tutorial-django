b4583ec14aae389dad7b0abc191367e0f6b72dd0
Bastien VINH <bus.bvinh@gmail.com>
2016-09-14 20:48:31 +0100

    Modify list

--- music/urls.py
+++ music/urls.py
@@ -6,11 +6,6 @@ app_name = 'music'
 urlpatterns = [
 
     # default page
-    url(r'^$', views.index, name='index'),
-
-    # Details page
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # Favourite page for a music
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite')
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail')
 ]
--- music/views.py
+++ music/views.py
@@ -1,35 +1,16 @@
-# from django.template import loader
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+from django.views import generic
+from .models import Album
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    # template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    # for album in all_albums:
-    #     url = "/music/{0}".format(album.id)
-    #     html += "<a href=\"{0}\">{1}</a><br />".format(url, album.album_title)
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
 
-def detail(request, album_id):
-    # album = Album.objects.get(pk=album_id)
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', { 'album': album })
 
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message' : "You did not selected a valid song"
-        })
-    else:
-        selected_song.is_favourite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', { 'album': album })
+    def get_queryset(self):
+        return Album.objects.all()
+
+
+class DetailView(generic.ListView):
+    model = Album
+    template_name = 'music/detail.html'
+    
